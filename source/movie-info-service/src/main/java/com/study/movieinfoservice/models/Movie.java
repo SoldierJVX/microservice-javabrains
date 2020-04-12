de75151c305030c6eadf23878df7869575848dbf
SoldierJVX <jv_rss@hotmail.com>
2020-04-12 14:59:49 -0300

    updated the model

--- source/movie-info-service/src/main/java/com/study/movieinfoservice/models/Movie.java
+++ source/movie-info-service/src/main/java/com/study/movieinfoservice/models/Movie.java
@@ -7,9 +7,14 @@ public class Movie {
 
     private String movieId;
     private String name;
+    private String description;
 
-    public Movie(String movieId, String name) {
+    public Movie() {
+    }
+
+    public Movie(String movieId, String name, String description) {
         this.movieId = movieId;
         this.name = name;
+        this.description = description;
     }
 }
--- source/movie-info-service/src/main/java/com/study/movieinfoservice/resources/MovieResource.java
+++ source/movie-info-service/src/main/java/com/study/movieinfoservice/resources/MovieResource.java
@@ -11,6 +11,6 @@ public class MovieResource {
 
     @RequestMapping("{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        return new Movie(movieId, "Test name");
+        return new Movie(movieId, "Test name","description");
     }
 }
