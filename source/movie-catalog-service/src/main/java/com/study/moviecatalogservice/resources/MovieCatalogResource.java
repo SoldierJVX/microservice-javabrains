8d3da57cf31332bfb3c4fd3d145c84aee09a707b
SoldierJVX <jv_rss@hotmail.com>
2020-04-12 15:00:52 -0300

    main microservice calls the movie-info-service microservice

--- source/movie-catalog-service/src/main/java/com/study/moviecatalogservice/resources/MovieCatalogResource.java
+++ source/movie-catalog-service/src/main/java/com/study/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,16 @@
 package com.study.moviecatalogservice.resources;
 
 import com.study.moviecatalogservice.models.CatalogItem;
+import com.study.movieinfoservice.models.Movie;
+import com.study.ratingsdataservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,9 +18,25 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Hello", 4)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // get all rated movie ids
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        // for each movie id, call movie info service and get details
+        // Put then all together
+        return ratings.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+
     }
 
 }
