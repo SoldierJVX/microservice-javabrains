ca98cf4fefa3d39a16a67f15b28f235069bbaa15
SoldierJVX <jv_rss@hotmail.com>
2020-04-12 14:21:22 -0300

    set the rating-data microservice

---
+++ source/ratings-data-service/src/main/java/com/study/ratingsdataservice/models/Rating.java
@@ -0,0 +1,15 @@
+package com.study.ratingsdataservice.models;
+
+import lombok.Data;
+
+@Data
+public class Rating {
+
+    private String movieId;
+    private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+}
